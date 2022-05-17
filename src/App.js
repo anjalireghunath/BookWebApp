52ddea181df2f3420ceddb84e8a29561c1e53fc8
anjalireghunath <anjalireghu98@gmail.com>
2022-05-17 12:29:50 +0530

    search book

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import Addbook from './components/Addbook';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Booksearch from './components/Booksearch';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
    <div>
-     <Addbook/>
-     <Booksearch/>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' exact element={<Addbook/>}/>
+      <Route path='/search' exact element={<Booksearch/>}/>
+    </Routes>
+    </BrowserRouter>
    </div>
   );
 }
