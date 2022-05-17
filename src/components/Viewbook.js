6c527a0922ccd70e4af344a1b45717831e290858
Anjali VR <79563933+anjalireghunath@users.noreply.github.com>
2022-05-17 19:35:07 +0530

    Update Viewbook.js

--- src/components/Viewbook.js
+++ src/components/Viewbook.js
@@ -27,9 +27,10 @@ const Viewbook = () => {
         <div className="container">
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl">
-            <div className="row g-5">
+            <div className="row g-3">
                {booklist.map((value,key)=>{
-                   return <div class="card">
+                   return <div className='col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4'>
+                   <div class="card">
                    <img src={value.image} class="card-img-top" alt="..."/>
                    <div class="card-body">
                      <h5 class="card-title">{value.title}</h5>
@@ -37,6 +38,7 @@ const Viewbook = () => {
                      <p class="card-text">Author:  {value.author}</p>
                      <a href="#" class="btn btn-primary">Buy Now</a>
                    </div>
+                 </div>
                  </div>
                })}
             </div>
